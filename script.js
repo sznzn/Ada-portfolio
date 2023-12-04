15b9425aee76c7cee49148b6bca3f36c8b8dd0dc
sznzn <lu0045@hotmail.com>
2023-12-04 12:40:16 +0100

    bureau

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ function timer() {
 ()/1000);
 }
 setInterval(timer, 1000);
+
+function clickme1() {
+    let projet1 = document.getElementById("projet1")
+    if(projet1.style.display == "none"){
+        projet1.style.display = "block"
+    }else{
+        projet1.style.display = "none"
+    }
+}
+function clickme2() {
+    let projet2 = document.getElementById("projet2")
+    if(projet2.style.display == "none"){
+        projet2.style.display = "block"
+    }else{
+        projet2.style.display = "none"
+    }
+}
+function clickme3() {
+    let projet3 = document.getElementById("projet3")
+    if(projet3.style.display == "none"){
+        projet3.style.display = "block"
+    }else{
+        projet3.style.display = "none"
+    }
+}
